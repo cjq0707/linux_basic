66ff96551bb4f2387d270b9834d144e307c117df
cjq0707 <cjq19890707@163.com>
2016-06-18 01:27:33 +0800

    modified:   client.c 	modified:   README.md 	modified:   server.c

--- client.c
+++ client.c
@@ -3,7 +3,7 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <assert.h>
-
+#define LEN 1024000
 int main()
 {
 	int sfd;	
@@ -16,11 +16,19 @@ int main()
 	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	
 	while(connect(sfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0);
-	char sendbuf[1024] = {0};
+	/*char sendbuf[1024] = {0};
 	char recvbuf[1024] = {0};
 	while(fgets(sendbuf, sizeof(sendbuf), stdin) != NULL)
 	{
 		write(sfd, sendbuf, strlen(sendbuf));
+	}*/
+	char *buf = malloc(LEN);
+	int ret;
+	while(1)
+	{
+		ret = send(sfd, buf, LEN, 0);
+		printf("sending data, ret %d\n", ret);
+		sleep(2);
 	}
 	return 0;
 }
